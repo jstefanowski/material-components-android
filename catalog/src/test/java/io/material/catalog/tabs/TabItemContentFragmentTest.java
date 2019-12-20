348f92d8a797944cfb8bc6da3276aaf98e5e461e
Jakub Stefanowski <jstefanowski@salesforce.com>
2019-12-20 16:37:37 +0100

    Removed not needed files.

---
+++ catalog/src/test/java/io/material/catalog/tabs/TabItemContentFragmentTest.java
@@ -0,0 +1,20 @@
+package io.material.catalog.tabs;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class TabItemContentFragmentTest {
+
+  @Test
+  public void test1() {
+    assertEquals(1,1);
+  }
+
+  @Test
+  public void test2() {
+    assertEquals(2,2);
+  }
+
+}
